8bce6c1bfe91626f018a48140de10e75e0bc59c4
Zoksss <zorantodorovic80@gmail.com>
2022-04-14 21:33:43 +0200

    inventory ui

--- public/js/sceneManager.js
+++ public/js/sceneManager.js
@@ -4,22 +4,45 @@ const gameplaySection = document.querySelector("#gameplaySection");
 
 const playPage = document.querySelector("#playPage");
 const shopPage = document.querySelector("#shopPage");
+const inventoryPage = document.querySelector("#inventoryPage");
+
 
 const navPlayBtn = document.querySelector("#navPlayBtn");
 const navShopBtn = document.querySelector("#navShopBtn");
+const navInventorypBtn = document.querySelector("#navInventorypBtn");
 
 const playAgainBtn = document.querySelector("#playAgainBtn");
 
 const transitionHolder = document.querySelector("#transitionHolder");
 
+const startSingleplayerBtn = document.querySelector("#startSingleplayerBtn");
+const goToMenuBtn = document.querySelector("#goToMenuBtn");
+
+const mainMenuSection = document.querySelector("#mainMenuSection");
+/*
+startSingleplayerBtn.addEventListener("click", () => {
+    transitionHolder.classList.add("transition");
+    transitionHolder.addEventListener("animationstart", () => {
+        let l = setTimeout(() => {
+            mainMenuSection.style.display = "none";
+            gameplaySection.style.display = "block";
+            clearTimeout(l);
+        }, 200);
+    })
+    transitionHolder.addEventListener('animationend', () => { transitionHolder.classList.remove("transition"); });
+    startGame();
+
+});
+
 navPlayBtn.addEventListener("click", () => {
     transitionHolder.classList.add("transition");
     transitionHolder.addEventListener("animationstart", () => {
-        setTimeout(() => {
+        let l = setTimeout(() => {
             shopPage.style.display = "none";
             playPage.style.display = "flex";
             navPlayBtn.classList.add("active");
             navShopBtn.classList.remove("active");
+            clearTimeout(l);
         }, 200);
     })
     transitionHolder.addEventListener('animationend', () => { transitionHolder.classList.remove("transition"); });
@@ -29,11 +52,12 @@ navPlayBtn.addEventListener("click", () => {
 navShopBtn.addEventListener("click", () => {
     transitionHolder.classList.add("transition");
     transitionHolder.addEventListener("animationstart", () => {
-        setTimeout(() => {
+        let l = setTimeout(() => {
             playPage.style.display = "none";
             shopPage.style.display = "flex";
             navShopBtn.classList.add("active");
             navPlayBtn.classList.remove("active");
+            clearTimeout(l);
         }, 200);
     })
     transitionHolder.addEventListener('animationend', () => { transitionHolder.classList.remove("transition"); });
@@ -44,11 +68,11 @@ navShopBtn.addEventListener("click", () => {
 playAgainBtn.addEventListener("click", () => {
     transitionHolder.classList.add("transition");
     transitionHolder.addEventListener("animationstart", () => {
-        setTimeout(() => {
+        let l = setTimeout(() => {
             endScreen.style.display = "none";
             gameplaySection.style.display = "block";
             startCounter.parentElement.style.display = "flex";
-
+            clearTimeout(l);
         }, 200);
     })
     transitionHolder.addEventListener('animationend', () => { transitionHolder.classList.remove("transition"); });
@@ -56,8 +80,56 @@ playAgainBtn.addEventListener("click", () => {
     startGame();
 });
 
+*/
+
+
+startSingleplayerBtn.addEventListener("click", () => {
+    mainMenuSection.style.display = "none";
+    gameplaySection.style.display = "block";
+    startGame();
+
+});
+
+navPlayBtn.addEventListener("click", () => {
+    shopPage.style.display = "none";
+    playPage.style.display = "flex";
+    inventoryPage.style.display = "none";
+    navPlayBtn.classList.add("active");
+    navShopBtn.classList.remove("active");
+    navInventorypBtn.classList.remove("active");
+});
+
+navShopBtn.addEventListener("click", () => {
+    playPage.style.display = "none";
+    shopPage.style.display = "flex";
+    inventoryPage.style.display = "none";
+    navShopBtn.classList.add("active");
+    navPlayBtn.classList.remove("active");
+    navInventorypBtn.classList.remove("active");
+
+});
+
+navInventorypBtn.addEventListener("click", () => {
+    playPage.style.display = "none";
+    shopPage.style.display = "none";
+    inventoryPage.style.display = "flex";
+    navInventorypBtn.classList.add("active");
+    navPlayBtn.classList.remove("active");
+    navShopBtn.classList.remove("active");
+
+})
+
+
+playAgainBtn.addEventListener("click", () => {
+    endScreen.style.display = "none";
+    gameplaySection.style.display = "block";
+    startCounter.parentElement.style.display = "flex";
+
+    startGame();
+});
 goToMenuBtn.addEventListener("click", () => {
     mainMenuSection.style.display = "flex";
     endScreen.style.display = "none";
-    startCounter.parentElement.style.display = "flex";    
+    gameplaySection.style.display = "none";
+    startCounter.parentElement.style.display = "flex";
 });
