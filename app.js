aedd4896fdf7eb335427042c12bc843eb76d837e
Zoksss <zorantodorovic80@gmail.com>
2022-04-27 23:06:55 +0200

    server connecting p2p and stuff

--- app.js
+++ app.js
@@ -12,6 +12,54 @@ app.use(express.static("public"));
 // Socket setup
 const io = socketio(server);
 
+class Room {
+    constructor(leader) {
+        this.leader = leader;
+        this.isPlaying = false;
+    }
+}
+
+let rooms = {};
+
 io.on("connection", (socket) => {
     console.log("Made socket connection with id: " + socket.id);
+    socket.on("startJoining", () => {
+        let room = searchForEmptyRoom();
+        console.log(room);
+        if (room) {
+            // join socket
+            socket.join(room.leader);
+            rooms[room.leader].isPlaying = true;
+            console.log("joined");
+            io.in(room.leader).emit("startGame");
+        } else {
+            // create new room
+            rooms[socket.id] = new Room(socket.id);
+            socket.join(socket.id);
+            console.log("created");
+        }
+    });
+    socket.on("cancelSearch", () => {
+        
+    });
+    socket.on("multiplayerDisconnect", () => {
+
+    });
+    socket.on("averageSend", time => {
+        var room = io.sockets.adapter.sids[socket.id];
+        
+        //io.to(rooms[0]).emit("averageShare", time);
+        console.log(room);
+    });
 });
+
+const searchForEmptyRoom = () => {
+    let isFound = null;
+    for (const key in rooms) {
+        if (rooms[key].isPlaying === false) {
+            isFound = rooms[key];
+            break;
+        }
+    }
+    return isFound;
+}
